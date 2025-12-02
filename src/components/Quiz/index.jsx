c318fd82dc2524b9240953875fd199461c7db324
kavery-pm <kaverypandandamadappa@Mac.fritz.box>
2025-12-02 13:52:45 +0100

    added sorting logic for answers

--- src/components/Quiz/index.jsx
+++ src/components/Quiz/index.jsx
@@ -1,15 +1,30 @@
 import { useState } from "react";
 import questions from "../../questions";
+import QuizCompleted from '../../assets/quiz-complete.png';
 
 const Quiz = () => {
 
 
     const [userAnswers, setuserAnswers] = useState([]);
     const activeQuestionIndex = userAnswers.length;
+
+    const quizCompleted = activeQuestionIndex === questions.length;
+
     const handleSelectAnswer = (answer) => {
         console.log("Selected answer:", answer);
         setuserAnswers((prevUserAnsers) => [...prevUserAnsers, answer]);
     };
+    if (quizCompleted) {
+        return (
+            <div id="summary">
+                <img src={QuizCompleted} alt='quiyCompleted'></img>
+                <h2>Quiz complete</h2>
+            </div>
+        );
+    }
+    const shuffledAnswers = [...questions[activeQuestionIndex].answers];
+    const sortedAnswers = shuffledAnswers.sort(() => Math.random() - 0.5);
+
     return (
         <div id='quiz'>
 
@@ -17,7 +32,7 @@ const Quiz = () => {
             <div id='question'>
                 <h2>{questions[activeQuestionIndex].text}</h2>
                 <ul id='answers'>
-                    {questions[activeQuestionIndex].answers.map((answer) => {
+                    {sortedAnswers.map((answer) => {
                         return (
                             <li key={answer} className="answer"><button onClick={() => { handleSelectAnswer(answer) }}>{answer}</button></li>
                         )
